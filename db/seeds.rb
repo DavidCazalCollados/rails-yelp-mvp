bc20d099044460e82033c53aac7f05c02dec8754
David Cazal <da.cazal95@gmail.com>
2024-11-20 14:13:12 -0500

    Seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+# 2. Create the instances 🏗️
+restaurants = [
+  {name: 'mcdconald', address: '123 rue st laurent', phone_number: '436667458', category: 'italian'},
+  {name: 'A&W', address: '345 rue st denis', phone_number: '3844475564', category: 'italian'},
+  {name: 'Belle province', address: '495 rue villeneuve', phone_number: '4557830048', category: 'japanese'},
+  {name: 'frites alors', address: '4985 rue belanger', phone_number: '5768894756', category: 'belgian'},
+  {name: 'Azalea', address: '475 rue de la roche', phone_number: '5768894756', category: 'french'}
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create!(restaurant)
+end
+
+# 3. Display a message 🎉
+puts "Finished! Created #{Restaurant.count} restaurants."
